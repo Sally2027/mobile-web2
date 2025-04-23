2a85d52c150a80461aa0cdc9d01f5abde38a5b68
Sally2027 <sallyalazab123@gmail.com>
2025-04-23 18:19:45 +0000

    working on the project

--- final-project/assets/js/script.js
+++ final-project/assets/js/script.js
@@ -1,39 +1,19 @@
 $(document).ready(function() {
     $('.slider').slick({
       centerMode: true,
-      slidesToShow: 4,
+      slidesToShow: 1,
       autoplay: true,
       autoplaySpeed: 3000,
-      arrows: true,
+      arrows: false,
       dots: true,
       infinite: true,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            centerMode: false,
-            slidesToShow: 1
-          }
-        }
-      ]
+      
       
     });
   });
 
-
   function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    const hamburgerIcon = document.querySelector('.hamburger i');
-  
-    navLinks.classList.toggle('active');
-  
-    // Toggle icon between bars and X
-    if (navLinks.classList.contains('active')) {
-      hamburgerIcon.classList.remove('fa-bars');
-      hamburgerIcon.classList.add('fa-x');
-    } else {
-      hamburgerIcon.classList.add('fa-bars');
-      hamburgerIcon.classList.remove('fa-x');
-    }
+    document.querySelector(".nav-links").classList.toggle("active");
   }
+  
   
