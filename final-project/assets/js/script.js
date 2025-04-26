843489a9bcebe93ba0d5a12c54bb002a3f764815
Sally2027 <sallyalazab123@gmail.com>
2025-04-26 00:13:11 +0000

    working on the final

--- final-project/assets/js/script.js
+++ final-project/assets/js/script.js
@@ -1,16 +1,25 @@
-$(document).ready(function() {
-    $('.slider').slick({
-      centerMode: true,
-      slidesToShow: 1,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      arrows: false,
-      dots: true,
-      infinite: true,
-      
-      
-    });
+$(document).ready(function () {
+  $('.slider').slick({
+    centerMode: true,
+    slidesToShow: 2, // ✅ default is desktop (769px and up)
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 768, // 768px and below
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   });
+});
+
+
+
 
   function toggleMenu() {
     document.querySelector(".nav-links").classList.toggle("active");
